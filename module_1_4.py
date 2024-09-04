4c784087e9ef5bedfce69bf7e21593f4bafe8012
Alexandr Shevchenko <djpestro@gmail.com>
2024-09-04 15:12:04 +0600

    edit module_1_4

---
+++ module_1_4.py
@@ -0,0 +1,3 @@
+my_string = input()
+print(f'В введёной Вами строке = {len(my_string)} символов')
+print(my_string.upper(), my_string.lower(), my_string.replace(' ', ''), my_string[0], my_string[-1], sep = '\n')
