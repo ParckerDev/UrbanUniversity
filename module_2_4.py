772a7713e0df1051bcce302c246a49884ad0bad5
Alexandr Shevchenko <djpestro@gmail.com>
2024-09-05 12:05:03 +0600

    continue edit module_2_4 and creat gitignore file

--- module_2_4.py
+++ module_2_4.py
@@ -1,6 +1,11 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = numbers[1:]
 not_primes = []
-for num in primes:
+for num in numbers[1:]:
     for i in range(2, num):
-        print(f'{num} % {i} = {num % i}')
+        if num % i == 0:
+            primes.remove(num)
+            not_primes.append(num)
+            break
+print(f'Primes: {primes}')
+print(f'Not Primes: {not_primes}')
