82b3c5733306b8c8cb049dc070ad9b845a218299
Alexandr Shevchenko <djpestro@gmail.com>
2024-09-10 18:14:16 +0600

    add module and continue edit

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as f_divide
+from true_math import divide as t_divide
+
+
+print(f_divide(34, 5))
+print(f_divide(990303, 345))
+print(f_divide(2, 9))
+print(f_divide(7, 0))
+print(t_divide(34, 6))
+print(t_divide(4, 8))
+print(t_divide(123434, 562))
+print(t_divide(99, 0))
