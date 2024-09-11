cab828221e5c6a8f21eae5a02f2b0b051b85f661
Alexandr Shevchenko <djpestro@gmail.com>
2024-09-11 21:41:01 +0600

    create and edit module_5_2

---
+++ module_5_2.py
@@ -0,0 +1,28 @@
+class House:
+    def __init__(self, name: str, number_of_floors: int) -> None:
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print('Такого этажа не существует')
+        else:
+            return [print(i) for i in range(1, new_floor + 1)]
+        
+    def __len__(self):
+        return self.number_of_floors
+    
+    def __str__(self) -> str:
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+    
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
