834ce9b90036f6769adae9d87a0aa8f4dd043886
Alexandr Shevchenko <djpestro@gmail.com>
2024-09-25 19:55:29 +0600

    добавил тесты

--- module_7_2.py
+++ module_7_2.py
@@ -6,3 +6,14 @@ def custom_write(file_name: str, strings: list):
             file.write(string + '\n')
     return strings_positions
 
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
