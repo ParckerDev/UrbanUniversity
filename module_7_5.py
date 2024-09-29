3c29e9ed9e2f08895b79db196d40fdfdf8c9b9b2
Alexandr Shevchenko <djpestro@gmail.com>
2024-09-29 21:58:58 +0600

    обернул код в функцию

--- module_7_5.py
+++ module_7_5.py
@@ -1,13 +1,13 @@
 import os
 import time
 
-directory = '.'
-for root, dirs, files in os.walk(directory):
-    for file in files:
-        file_path = os.path.join(root, file)
-        filesize = os.path.getsize(file_path)
-        filetime = os.path.getmtime(file)
-        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-        parrent_directory = os.path.basename(root)
+def scan_folder(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            filesize = os.path.getsize(file_path)
+            filetime = os.path.getmtime(file)
+            formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+            parrent_directory = os.path.basename(root)
 
-        print(f'Обнаружен файл: {file}, Путь: {file_path}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parrent_directory}')
+            print(f'Обнаружен файл: {file}, Путь: {file_path}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parrent_directory}')
