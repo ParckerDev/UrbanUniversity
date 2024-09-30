eaff993d4bc478882c83218b7c92b0c32548faaf
Alexandr Shevchenko <djpestro@gmail.com>
2024-09-30 17:52:35 +0600

    убрал из функции

--- module_7_5.py
+++ module_7_5.py
@@ -1,15 +1,13 @@
 import os
 import time
 
-def scan_folder(directory):
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            file_path = os.path.join(root, file)
-            filesize = os.path.getsize(file_path)
-            filetime = os.path.getmtime(file_path)
-            formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-            parrent_directory = os.path.dirname(file_path)
+directory = '.'
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        file_path = os.path.join(root, file)
+        filesize = os.path.getsize(file_path)
+        filetime = os.path.getmtime(file_path)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        parrent_directory = os.path.dirname(file_path)
+        print(f'Обнаружен файл: {file}, Путь: {file_path}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parrent_directory}')
 
-            print(f'Обнаружен файл: {file}, Путь: {file_path}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parrent_directory}')
-
-scan_folder('.')
