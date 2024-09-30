3e64b50b2227e1210a07a39edd60d3a98490a082
Alexandr Shevchenko <djpestro@gmail.com>
2024-09-30 19:00:15 +0600

    add __iter__ and __next__

--- module_9_5.py
+++ module_9_5.py
@@ -1,4 +1,29 @@
 class StepValueError(ValueError):
     pass
 
+class Iterator():
+    def __init__(self, start, stop, step=1):
+        self.start = start
+        self.stop = stop
+        if step != 0:
+            self.step = step
+        else:
+            raise StepValueError ('шаг не может быть равен 0')
+        self.pointer = start
 
+    def __iter__(self):
+        self.pointer = self.start
+        return self
+    
+    def __next__(self):
+        self.pointer += self.step
+        if self.step > 0 and self.pointer <= self.stop:
+            return self.pointer
+        if self.step < 0 and self.pointer >= self.stop:
+            return self.pointer
+        else:
+            return "The end"
+        raise StopIteration()
+    
+
+    
