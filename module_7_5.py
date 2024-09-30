8d0c0c02b1244394c0f9550b9674b4bab80adb79
Alexandr Shevchenko <djpestro@gmail.com>
2024-09-30 15:43:41 +0600

    edit

--- module_7_5.py
+++ module_7_5.py
@@ -8,7 +8,7 @@ def scan_folder(directory):
             filesize = os.path.getsize(file_path)
             filetime = os.path.getmtime(file_path)
             formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-            parrent_directory = os.path.basename(file_path)
+            parrent_directory = os.path.dirname(file_path)
 
             print(f'Обнаружен файл: {file}, Путь: {file_path}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parrent_directory}')
 
