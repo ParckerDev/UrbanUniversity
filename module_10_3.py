94bf88fe579c492848ea297c53f5b725db72231a
Alexandr Shevchenko <djpestro@gmail.com>
2024-10-01 20:29:34 +0300

    add method take, + tests

--- module_10_3.py
+++ module_10_3.py
@@ -1,5 +1,5 @@
 import random
-from threading import Lock
+from threading import Lock, Thread
 from time import sleep
 
 
@@ -16,3 +16,30 @@ class Bank:
             if self.balance >= 500 and self.lock.locked():
                 self.lock.release()
             sleep(0.001)
+
+    def take(self):
+        for _ in range(100):
+            value_take = random.randrange(50, 100)
+            print(f'Запрос на {value_take}')
+            if value_take <= self.balance:
+                self.balance -= value_take
+                print(f'Снятие: {value_take}. Баланс: {self.balance}')
+            else:
+                print(f'Запрос отклонён, недостаточно средств')
+                self.lock.acquire()
+
+
+# TESTS
+
+bank = Bank()
+
+deposit = Thread(target=bank.deposit)
+take = Thread(target=bank.take)
+
+deposit.start()
+take.start()
+
+deposit.join()
+take.join()
+
+print(f'Итоговый баланс: {bank.balance}')
