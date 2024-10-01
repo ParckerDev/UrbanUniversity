0b958899c0f06b58d9494ad603b3ea3633d325ff
Alexandr Shevchenko <djpestro@gmail.com>
2024-10-01 19:16:43 +0600

    add method run

--- module_10_2.py
+++ module_10_2.py
@@ -1,10 +1,22 @@
 from threading import Thread
+from time import sleep
 
 
 class Knight(Thread):
+    
     def __init__(self, name, power):
         self.name = name
         self.power = power
         super().__init__()
+        
 
-    
+    def run(self):
+        print(f'{self.name}, на нас напали!')
+        enemies = 100
+        days = 0
+        while enemies > 0:
+            enemies -= self.power
+            sleep(1)
+            days += 1
+            print(f'{self.name} сражается {days}..., осталось {enemies} воинов.')
+        print(f'{self.name} одержал победу спустя {days} дней(дня)!')
