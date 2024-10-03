60b8f9c4a273555c0a6e4ba6cf2b645325616bc3
Alexandr Shevchenko <djpestro@gmail.com>
2024-10-03 21:57:23 +0600

    add tests

--- module_10_4.py
+++ module_10_4.py
@@ -38,3 +38,24 @@ class Cafe:
 
     def discuss_guests(self, ):
         pass
+
+
+
+
+# TESTS
+
+# Создание столов
+tables = [Table(number) for number in range(1, 6)]
+# Имена гостей
+guests_names = [
+'Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
+'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra'
+]
+# Создание гостей
+guests = [Guest(name) for name in guests_names]
+# Заполнение кафе столами
+cafe = Cafe(*tables)
+# Приём гостей
+cafe.guest_arrival(*guests)
+# Обслуживание гостей
+cafe.discuss_guests()
