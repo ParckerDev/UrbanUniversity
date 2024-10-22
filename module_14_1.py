f6a579c0ac4bb701862f324b562c9c7e933ef5a8
Alexandr Shevchenko <djpestro@gmail.com>
2024-10-22 22:13:07 +0600

    continue edit

--- module_14_1.py
+++ module_14_1.py
@@ -13,8 +13,21 @@ CREATE TABLE IF NOT EXISTS Users(
                )
 ''')
 
-cursor.execute(" CREATE INDEX idx_email ON Users (email)")
+# Create INDEX with email
+cursor.execute(" CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
 
+# Create users
+for i in range(1,11):
+    cursor.execute(" INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)", (f'User{i}', f'example{i}@gmail.com', f'{i*10}', 1000))
+
+# Update balance
+cursor.execute(" UPDATE Users SET balance = 500 WHERE id % 2 != 0")
+
+# Delete Users
+cursor.execute(" SELECT * FROM Users")
+users = cursor.fetchall()
+for i in range(1, len(users) + 1, 3):
+    cursor.execute(" DELETE FROM Users WHERE id = ?", (f'{i}',))
 
 connection.commit()
 connection.close()
