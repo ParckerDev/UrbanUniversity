dda620fabef94dc7cae471c31f66bda3edbdec13
Alexandr Shevchenko <djpestro@gmail.com>
2024-10-22 21:10:17 +0600

    create cursor, create table code, close connection and commit

--- module_14_1.py
+++ module_14_1.py
@@ -1,3 +1,18 @@
 import sqlite3
 
-connection = sqlite3.connect('database.db')
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXIST Users(
+               id INTEGER PRIMARY KEY,
+               username TEXT NOT NULL,
+               email TEXT NOT NULL,
+               age INTEGER,
+               balance INTEGER NOT NULL
+               )
+''')
+
+
+connection.commit()
+connection.close()
