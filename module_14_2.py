ff0c3e0a850af9c9850f25141f25f5cd7690939e
Alexandr Shevchenko <djpestro@gmail.com>
2024-10-23 10:20:57 +0600

    del user where id = 6

--- module_14_2.py
+++ module_14_2.py
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS Users(
 
 # Create INDEX with email
 cursor.execute(" CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
-
+'''
 # Create users
 for i in range(1,11):
     cursor.execute(" INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)", (f'User{i}', f'example{i}@gmail.com', f'{i*10}', 1000))
@@ -28,6 +28,14 @@ cursor.execute(" SELECT * FROM Users")
 users = cursor.fetchall()
 for i in range(1, len(users) + 1, 3):
     cursor.execute(" DELETE FROM Users WHERE id = ?", (f'{i}',))
+
+# Delete User where id = 6
+cursor.execute(" DELETE FROM Users WHERE id = 6")
+'''
+# Count records
+cursor.execute('SELECT COUNT(*) FROM Users')
+count_users = cursor.fetchone()[0]
+print(count_users)
 
 connection.commit()
 connection.close()
