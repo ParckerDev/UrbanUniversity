5760270e70998b7ed9111389c7e8e1e189396c1d
Alexandr Shevchenko <djpestro@gmail.com>
2024-10-23 10:12:52 +0600

    create module_14_2 and copy paste previos code

---
+++ module_14_2.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+               id INTEGER PRIMARY KEY,
+               username TEXT NOT NULL,
+               email TEXT NOT NULL,
+               age INTEGER,
+               balance INTEGER NOT NULL
+               )
+''')
+
+# Create INDEX with email
+cursor.execute(" CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
+
+# Create users
+for i in range(1,11):
+    cursor.execute(" INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)", (f'User{i}', f'example{i}@gmail.com', f'{i*10}', 1000))
+
+# Update balance
+cursor.execute(" UPDATE Users SET balance = 500 WHERE id % 2 != 0")
+
+# Delete Users
+cursor.execute(" SELECT * FROM Users")
+users = cursor.fetchall()
+for i in range(1, len(users) + 1, 3):
+    cursor.execute(" DELETE FROM Users WHERE id = ?", (f'{i}',))
+
+connection.commit()
+connection.close()
