2aa2c1f1ee35ad1e8c8fa58eedd893fda15fd433
Alexandr Shevchenko <djpestro@gmail.com>
2024-10-24 16:55:18 +0300

    continue edit

--- module_16_1.py
+++ module_16_1.py
@@ -5,19 +5,23 @@ import uvicorn
 app = FastAPI()
 
 @app.get('/')
-async def welcome():
+async def get_main_page():
     return {'message': 'Главная страница'}
 
 @app.get('/user/admin')
-async def admin():
+async def get_admin_page():
     return {'message': 'Вы вошли как администратор'}
 
 @app.get('/user/{user_id}')
-async def user(user_id):
+async def get_user_id(user_id):
     return {'message': f'Вы вошли как пользователь № {user_id}'}
+
+@app.get('/user')
+async def get_user_info(username: str = 'Ilya', age: int = 26):
+    return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
 
 
 
 
 if __name__ == "__main__":
-    uvicorn.run("tmp:app", reload=True)
+    uvicorn.run("module_16_1:app", reload=True)
