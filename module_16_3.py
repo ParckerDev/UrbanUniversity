8e27bf2c3e96d4d602e09d87e203108951f7a2f9
Alexandr Shevchenko <djpestro@gmail.com>
2024-10-25 00:33:21 +0600

    continue edit

--- module_16_3.py
+++ module_16_3.py
@@ -2,26 +2,31 @@ from typing import Annotated
 from fastapi import FastAPI, Path
 import uvicorn
 
-
+users = {'1': 'Имя: Example, возраст: 18'}
 app = FastAPI()
 
-@app.get('/')
-async def get_main_page():
-    return {'message': 'Главная страница'}
+@app.get('/users')
+async def get_users():
+    return users
 
-@app.get('/user/admin')
-async def get_admin_page():
-    return {'message': 'Вы вошли как администратор'}
+@app.post('/user/{username}/{age}')
+async def add_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
+                   age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]):
+    current_id = int(max(users.keys(), key=int)) + 1
+    users[current_id] = f'Имя: {username}, возраст: {age}'
+    return f'User {current_id} is registered'
 
-@app.get('/user/{user_id}')
-async def get_user_id(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='34')]):
-    return {'message': f'Вы вошли как пользователь № {user_id}'}
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='34')],
+                      username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
+                      age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]):
+    users[user_id] = f'Имя: {username}, возраст: {age}'
+    return f'The user {user_id} is update'
 
-@app.get('/user/{username}/{age}')
-async def get_user_info(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
-                        age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]):
-    return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
-
+@app.delete('/user/{user_id}')
+async def delete_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='34')]):
+    del users[user_id]
+    return f'User {user_id} has been deleted'
 
 
 
