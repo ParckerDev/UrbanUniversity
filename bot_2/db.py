15012d9b9884483b8ec2c190d5866cc0d8d55978
Alexandr Shevchenko <djpestro@gmail.com>
2024-10-24 11:34:29 +0600

    add orm function on db.py

--- bot_2/db.py
+++ bot_2/db.py
@@ -1,3 +1,4 @@
+from email import message
 import sqlite3
 
 connection = sqlite3.connect('not_telegram.db')
@@ -12,7 +13,7 @@ CREATE TABLE IF NOT EXISTS Users(
         )
 ''')
 
-
+# Добавление юзера
 def add_user(user_id, username, first_name):
     check_user = cursor.execute('SELECT * FROM Users WHERE id = ?', (user_id,))
     if check_user.fetchone() is None:
@@ -20,6 +21,37 @@ def add_user(user_id, username, first_name):
             INSERT INTO Users VALUES ('{user_id}', '{username}', '{first_name}', 0)
         ''')
     connection.commit()
+
+# Показать всех юзеров
+def show_users():
+    users_list = cursor.execute("SELECT * FROM Users")
+    message = ''
+    for user in users_list:
+        message += f'{user[0]} @{user[1]} {user[2]}\n'
+    connection.commit()
+    return message
+
+# Показать количество пользователей
+def show_stat():
+    users_count = cursor.execute("SELECT COUNT(*) FROM Users").fetchone()
+    connection.commit()
+    return users_count[0]
+
+# Добавление юзера в блок
+def add_to_block(user_id):
+    cursor.execute(f"UPDATE Users SET block = 1 WHERE user_id = {user_id}")
+    connection.commit()
+
+# Удаление юзера из блока
+def add_to_block(user_id):
+    cursor.execute(f"UPDATE Users SET block = 0 WHERE user_id = {user_id}")
+    connection.commit()
+
+# Проверка есть ли юзер в блоке
+def check_block_user(user_id):
+    return False if cursor.execute(f'SELECT block FROM Users WHERE user_id = {user_id}').fetchone()[0] == 0 else True
+     
+
 
 connection.commit()
 connection.close()
