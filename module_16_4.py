8b3051fdbce27026eb3ff99eb18f48a128f37d9f
Alexandr Shevchenko <djpestro@gmail.com>
2024-10-28 23:07:15 +0600

    update code

--- module_16_4.py
+++ module_16_4.py
@@ -18,24 +18,30 @@ async def get_users():
 
 @app.post('/user/{username}/{age}')
 async def add_user(user: User):
-    current_id = 1 if not users else users[-1].id + 1
-    users.append({'id': current_id, 'username': user.username, 'age': user.age})
-    return {'id': current_id, 'username': user.username, 'age': user.age}
+    user.id = 1 if not users else users[-1].id + 1
+    users.append(user)
+    return user
 
 @app.put('/user/{user_id}/{username}/{age}')
 async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='34')],
                       username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
                       age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]):
     try:
-        users[user_id] = {'id': current_id, 'username': user.username, 'age': user.age}
+        updated_user = list(filter(lambda user: user.id == user_id, users))[0]
+        updated_user.username = username
+        updated_user.age = age
         return f'The user {user_id} is update'
     except IndexError:
-        raise HTTPException(status_code=404, detail='User not found')
+        raise HTTPException(status_code=404, detail='User was not found')
 
 @app.delete('/user/{user_id}')
 async def delete_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='34')]):
-    del users[user_id]
-    return f'User {user_id} has been deleted'
+    try:
+        deleted_user = list(filter(lambda user: user.id == user_id, users))[0]
+        users.remove(deleted_user)
+        return deleted_user
+    except IndexError:
+        raise HTTPException(status_code=404, detail='User was not found')
 
 
 
