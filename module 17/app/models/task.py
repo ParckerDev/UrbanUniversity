3153350097181bed57e1fd8209697e7e40cb67da
Alexandr Shevchenko <djpestro@gmail.com>
2024-10-30 12:17:08 +0600

    from task and user model import modules

---
+++ module 17/app/models/task.py
@@ -0,0 +1,5 @@
+from app.backend.db import Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
+
+
+
---
+++ module 17/app/models/user.py
@@ -0,0 +1,2 @@
+from app.backend.db import Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
