cda03e16a3161ec271aa524c3c28282904bca12a
Alexandr Shevchenko <djpestro@gmail.com>
2024-10-30 16:09:43 +0600

    create model user and task

--- module 17/app/models/task.py
+++ module 17/app/models/task.py
@@ -1,5 +1,17 @@
-from app.backend.db import Base
+from models import Base
+import user
 from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
 
 
-
+class Task(Base):
+    __tablename__ = 'tasks'
+    __table_args__ = {'keep_existing': True}
+    id = Column(Integer, primary_key=True, index=True)#- целое число, первичный ключ, с индексом.
+    title = Column(String) #- строка.
+    content = Column(String) #- строка.
+    priority = Column(Integer, default=0) #- целое число, по умолчанию 0.
+    completed = Column(Boolean, default=False) #- булевое значение, по умолчанию False.
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False, index=True) #- целое число, внешний ключ на id из таблицы 'users', не NULL, с индексом.
+    slug = Column(String, unique=True, index=True) # - строка, уникальная, с индексом.
+    user = relationship('User', back_populates='tasks') # - объект связи с таблицей с таблицей User, где back_populates='task
--- module 17/app/models/user.py
+++ module 17/app/models/user.py
@@ -1,2 +1,21 @@
 from app.backend.db import Base
+import task
 from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
+
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True) # - целое число, первичный ключ, с индексом.
+    username = Column(String) # - строка.
+    firstname = Column(String) #- строка.
+    lastname = Column(String) #- строка.
+    age = Column(Integer) #- целое число.
+    slug = Column(String, unique=True, index=True) #- строка, уникальная, с индексом.
+    tasks = relationship('Task', back_populates='user') # - объект связи с таблицей с таблицей Task, где back_populates='user'.
+
+
+
+from sqlalchemy.schema import CreateTable
+print(CreateTable(User.__table__))
